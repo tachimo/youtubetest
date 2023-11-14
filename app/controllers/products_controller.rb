4672773ae9a91e2b34f325211a8c1fd304474e6a
tachimo <shihon17@gmail.com>
2023-11-14 23:56:05 +0900

    Add:コントローラーの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,34 @@
 class ProductsController < ApplicationController
   def index
-    @products Product.all
+    @products = Product.all
+  end
+ 
+  def new
+    @products = Product.new
+  end
+
+  def create
+    product.create(product_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    product = product.find(params[:id])
+    product.destroy
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    product.update(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :youtube_url)
   end
 end
